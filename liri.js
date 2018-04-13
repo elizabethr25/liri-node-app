2f7a46842708546ff912534d4b460cb3d6400625
Elizabeth Richardson <emrichardson820@gmail.com>
2018-04-13 10:16:05 -0700

    added code / tokens

--- liri.js
+++ liri.js
@@ -1,1 +1,15 @@
 require("dotenv").config();
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+switch(process.argv[2]){
+    case `my-tweets` : 
+    tweets(); break;
+    case `spotify-this-song`:
+    spotify(); break;
+    case `movie-this` :
+    movie(); break;
+    case `do-what-it-says` :
+    doWhat(); break;
+}
