1e8ccb3179c73c6150c97e5357932addeb8dc159
Elizabeth Richardson <emrichardson820@gmail.com>
2018-04-14 14:21:21 -0700

    twitter functionality added

--- liri.js
+++ liri.js
@@ -1,21 +1,62 @@
 require("dotenv").config();
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var fs = require("fs");
+const Twitter = require("twitter");
+const Spotify = require("node-spotify-api");
+const keys = require('./keys.js')
 
-switch(process.argv[2]){
-    case `my-tweets` : 
-    tweets(); break;
-    case `spotify-this-song`:
-    spotify(); break;
-    case `movie-this` :
-    movie(); break;
-    case `do-what-it-says` :
-    doWhat(); break;
+const spotify = new Spotify(keys.spotify);
+const client = new Twitter(keys.twitter);
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+switch (arg1) {
+    case 'my-tweets':
+        tweets();
+        break;
+    case 'spotify-this-song':
+        spotify();
+        break;
+    case 'movie-this':
+        movie();
+        break;
+    case 'do-what-it-says':
+        doWhat();
+        break;
+    case '':
+        if (arg2 === 'undefined') {
+            console.log(`You requested ${arg2}`);
+        }
+        else {
+            console.log('Defaulting to The Sign');
+        }
+
+    default:
+        console.log("We don't know what you're taking about.")
 }
 
-function tweets() { 
-    
+
+//Twitter
+function tweets() {
+    var screenName = { screen_name: 'Elizabe47127458' };
+    client.get('statuses/user_timeline', screenName, function (error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                console.log('Elizabeth tweeted: ' + tweets[i].text + " at " + tweets[i].created_at);
+                fs.appendFile('log.txt', 'Elizabeth tweeted: ' + tweets[i].text + " at " + tweets[i].created_at);
+            }
+        }
+        else {
+            console.log('Error.')
+        }
+
+    });
+
+
+//Spotify
+function spotify () {}
+
 }
 
 
