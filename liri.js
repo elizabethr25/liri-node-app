871c04acffd66391d61667fbbf1a4965b3fdac64
Elizabeth Richardson <emrichardson820@gmail.com>
2018-04-14 15:15:04 -0700

    spotify added

--- liri.js
+++ liri.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
+require('dotenv').config();
 
-var fs = require("fs");
-const Twitter = require("twitter");
-const Spotify = require("node-spotify-api");
+var fs = require('fs');
+const Twitter = require('twitter');
+const Spotify = require('node-spotify-api');
 const keys = require('./keys.js')
 
 const spotify = new Spotify(keys.spotify);
@@ -11,12 +11,25 @@ const client = new Twitter(keys.twitter);
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
+var x = "";
+for (var i = 3; i < process.argv.length; i++) {
+    if (i > 3 && i < process.argv.length) {
+        x = x + "+" + process.argv[i];
+    } else {
+        x = x + process.argv[i];
+    }
+}
+
 switch (arg1) {
     case 'my-tweets':
         tweets();
         break;
     case 'spotify-this-song':
-        spotify();
+        if (x) {
+            spotifySong(x);
+        } else {
+            spotifySong('The Sign');
+        }
         break;
     case 'movie-this':
         movie();
@@ -41,9 +54,12 @@ switch (arg1) {
 function tweets() {
     var screenName = { screen_name: 'Elizabe47127458' };
     client.get('statuses/user_timeline', screenName, function (error, tweets, response) {
+        // console.log(JSON.stringify(data));
         if (!error) {
             for (var i = 0; i < tweets.length; i++) {
+                //Console.log tweets 
                 console.log('Elizabeth tweeted: ' + tweets[i].text + " at " + tweets[i].created_at);
+                //Append tweets to log.txt file 
                 fs.appendFile('log.txt', 'Elizabeth tweeted: ' + tweets[i].text + " at " + tweets[i].created_at);
             }
         }
@@ -52,12 +68,34 @@ function tweets() {
         }
 
     });
-
+}
 
 //Spotify
-function spotify () {}
-
+function spotifySong(song) {
+    spotify.search({ type: 'track', query: song }, function (error, data) {
+        // console.log(JSON.stringify(data));
+        if (!error) {
+            for (var i = 0; i < data.tracks.items.length; i++) {
+                //Artists 
+                console.log('Artist: ' + data.tracks.items[i].artists[0].name);
+                fs.appendFile('log.txt', 'Artist: ' + data.tracks.items[i].artists[0].name);
+                //Song's name
+                console.log('Song name: ' + data.tracks.items[i].name);
+                fs.appendFile('log.txt', 'Song name: ' + data.tracks.items[i].name);
+                //Link to song
+                console.log('Link to song: ' + data.tracks.items[i].external_urls.spotify);
+                fs.appendFile('log.txt', 'Link to song' + data.tracks.items[i].external_urls.spotify);
+                //Album
+                console.log('Album: ' + data.tracks.items[i].album.name);
+                fs.appendFile('log.txt', 'Album: ' + data.tracks.items[i].album.name);
+            }
+        }
+        else {
+            return console.log('An error occurred:');
+        }
+    });
 }
+
 
 
 
